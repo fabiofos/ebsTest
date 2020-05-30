b979d817dea7f34ae02d13d4897181e23fbf0919
Fabio Oliveira <fabiooliveira@MBP-de-Fabio.Home>
2020-05-30 10:42:06 +0100

    jwt

--- ebsTest/src/app/app.module.ts
+++ ebsTest/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: function  tokenGetter() {
+             return     localStorage.getItem('token');},
+        whitelistedDomains: ['http://localhost:4200/'],
+        blacklistedRoutes: ['http://localhost:4200/auth/login']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
